fc879140eed7aa989186517801c8f952555c51f0
Admin <admin@Admins-MacBook-Pro.local>
2019-02-25 21:25:21 +0700

    Ok

---
+++ java/exportkit/xd/iphone_6_7_8___1_activity.java
@@ -0,0 +1,184 @@
+
+	 
+	/*
+	 *	This content is generated from the PSD File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_6_7_8___1
+	 *	@date 		0
+	 *	@title 		iPhone 6/7/8  1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.2.8.xd
+	 *
+	 */
+	
+
+package exportkit.xd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class iphone_6_7_8___1_activity extends Activity {
+
+	
+	private View _bg__iphone_6_7_8___1_ek2;
+	private TextView beranda;
+	private TextView _19_jumada_al_ula_1440_h;
+	private ImageView ic_share_24px;
+	private ImageView masjid_abu_darda;
+	private ImageView icon;
+	private View rectangle_1774;
+	private View rectangle_1775;
+	private TextView waktu_shalat_hari_ini;
+	private View rectangle_1776;
+	private View rectangle_1777;
+	private View rectangle_1778;
+	private ImageView path_1;
+	private ImageView ic_chevron_right_24px;
+	private TextView notifikasi_tidak_aktif;
+	private TextView senyapkan_adzan;
+	private TextView dzikir_shalat_fardhu;
+	private TextView dzikir_pagi_petang;
+	private TextView _15_26;
+	private TextView ashar;
+	private View rectangle_1780;
+	private TextView atur_lokasi;
+	private ImageView ic_gps_fixed_24px;
+	private View rectangle_1781;
+	private TextView kota_pekanbaru;
+	private ImageView ic_place_24px;
+	private ImageView compass_flat_icon_01_;
+	private View rectangle_1782;
+	private TextView _84;
+	private ImageView ic_notifications_active_24px;
+	private ImageView ic_notifications_off_24px;
+	private ImageView muslim_praying_hands;
+	private ImageView muslim_man_praying;
+	private TextView tanya_ustadz;
+	private View rectangle_1783;
+	private ImageView path_6;
+	private ImageView path_7;
+	private ImageView path_8;
+	private ImageView path_9;
+	private ImageView icon_ek1;
+	private View rectangle_1784;
+	private TextView offline;
+	private View rectangle_1785;
+	private TextView offline_ek1;
+	private View rectangle_1786;
+	private TextView on_call;
+	private View rectangle_1787;
+	private TextView online;
+	private View rectangle_1788;
+	private TextView a;
+	private View rectangle_1789;
+	private TextView ar;
+	private View rectangle_1790;
+	private TextView c;
+	private TextView info_aplikasi_halo_ustadz;
+	private TextView anas_burhanuddin__lc_ma;
+	private TextView dr__erwandi_tarmidzi__lc__ma;
+	private TextView dr__abdullah_roy__lc__ma;
+	private View rectangle_1791;
+	private ImageView ic_home_24px;
+	private TextView beranda_ek1;
+	private TextView ustadz;
+	private ImageView ic_supervisor_account_24px;
+	private ImageView ic_description_24px;
+	private TextView fatwa;
+	private ImageView ic_account_circle_24px;
+	private ImageView ic_track_changes_24px;
+	private TextView qiblat;
+	private TextView akun;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_6_7_8___1);
+
+		
+		_bg__iphone_6_7_8___1_ek2 = (View) findViewById(R.id._bg__iphone_6_7_8___1_ek2);
+		beranda = (TextView) findViewById(R.id.beranda);
+		_19_jumada_al_ula_1440_h = (TextView) findViewById(R.id._19_jumada_al_ula_1440_h);
+		ic_share_24px = (ImageView) findViewById(R.id.ic_share_24px);
+		masjid_abu_darda = (ImageView) findViewById(R.id.masjid_abu_darda);
+		icon = (ImageView) findViewById(R.id.icon);
+		rectangle_1774 = (View) findViewById(R.id.rectangle_1774);
+		rectangle_1775 = (View) findViewById(R.id.rectangle_1775);
+		waktu_shalat_hari_ini = (TextView) findViewById(R.id.waktu_shalat_hari_ini);
+		rectangle_1776 = (View) findViewById(R.id.rectangle_1776);
+		rectangle_1777 = (View) findViewById(R.id.rectangle_1777);
+		rectangle_1778 = (View) findViewById(R.id.rectangle_1778);
+		path_1 = (ImageView) findViewById(R.id.path_1);
+		ic_chevron_right_24px = (ImageView) findViewById(R.id.ic_chevron_right_24px);
+		notifikasi_tidak_aktif = (TextView) findViewById(R.id.notifikasi_tidak_aktif);
+		senyapkan_adzan = (TextView) findViewById(R.id.senyapkan_adzan);
+		dzikir_shalat_fardhu = (TextView) findViewById(R.id.dzikir_shalat_fardhu);
+		dzikir_pagi_petang = (TextView) findViewById(R.id.dzikir_pagi_petang);
+		_15_26 = (TextView) findViewById(R.id._15_26);
+		ashar = (TextView) findViewById(R.id.ashar);
+		rectangle_1780 = (View) findViewById(R.id.rectangle_1780);
+		atur_lokasi = (TextView) findViewById(R.id.atur_lokasi);
+		ic_gps_fixed_24px = (ImageView) findViewById(R.id.ic_gps_fixed_24px);
+		rectangle_1781 = (View) findViewById(R.id.rectangle_1781);
+		kota_pekanbaru = (TextView) findViewById(R.id.kota_pekanbaru);
+		ic_place_24px = (ImageView) findViewById(R.id.ic_place_24px);
+		compass_flat_icon_01_ = (ImageView) findViewById(R.id.compass_flat_icon_01_);
+		rectangle_1782 = (View) findViewById(R.id.rectangle_1782);
+		_84 = (TextView) findViewById(R.id._84);
+		ic_notifications_active_24px = (ImageView) findViewById(R.id.ic_notifications_active_24px);
+		ic_notifications_off_24px = (ImageView) findViewById(R.id.ic_notifications_off_24px);
+		muslim_praying_hands = (ImageView) findViewById(R.id.muslim_praying_hands);
+		muslim_man_praying = (ImageView) findViewById(R.id.muslim_man_praying);
+		tanya_ustadz = (TextView) findViewById(R.id.tanya_ustadz);
+		rectangle_1783 = (View) findViewById(R.id.rectangle_1783);
+		path_6 = (ImageView) findViewById(R.id.path_6);
+		path_7 = (ImageView) findViewById(R.id.path_7);
+		path_8 = (ImageView) findViewById(R.id.path_8);
+		path_9 = (ImageView) findViewById(R.id.path_9);
+		icon_ek1 = (ImageView) findViewById(R.id.icon_ek1);
+		rectangle_1784 = (View) findViewById(R.id.rectangle_1784);
+		offline = (TextView) findViewById(R.id.offline);
+		rectangle_1785 = (View) findViewById(R.id.rectangle_1785);
+		offline_ek1 = (TextView) findViewById(R.id.offline_ek1);
+		rectangle_1786 = (View) findViewById(R.id.rectangle_1786);
+		on_call = (TextView) findViewById(R.id.on_call);
+		rectangle_1787 = (View) findViewById(R.id.rectangle_1787);
+		online = (TextView) findViewById(R.id.online);
+		rectangle_1788 = (View) findViewById(R.id.rectangle_1788);
+		a = (TextView) findViewById(R.id.a);
+		rectangle_1789 = (View) findViewById(R.id.rectangle_1789);
+		ar = (TextView) findViewById(R.id.ar);
+		rectangle_1790 = (View) findViewById(R.id.rectangle_1790);
+		c = (TextView) findViewById(R.id.c);
+		info_aplikasi_halo_ustadz = (TextView) findViewById(R.id.info_aplikasi_halo_ustadz);
+		anas_burhanuddin__lc_ma = (TextView) findViewById(R.id.anas_burhanuddin__lc_ma);
+		dr__erwandi_tarmidzi__lc__ma = (TextView) findViewById(R.id.dr__erwandi_tarmidzi__lc__ma);
+		dr__abdullah_roy__lc__ma = (TextView) findViewById(R.id.dr__abdullah_roy__lc__ma);
+		rectangle_1791 = (View) findViewById(R.id.rectangle_1791);
+		ic_home_24px = (ImageView) findViewById(R.id.ic_home_24px);
+		beranda_ek1 = (TextView) findViewById(R.id.beranda_ek1);
+		ustadz = (TextView) findViewById(R.id.ustadz);
+		ic_supervisor_account_24px = (ImageView) findViewById(R.id.ic_supervisor_account_24px);
+		ic_description_24px = (ImageView) findViewById(R.id.ic_description_24px);
+		fatwa = (TextView) findViewById(R.id.fatwa);
+		ic_account_circle_24px = (ImageView) findViewById(R.id.ic_account_circle_24px);
+		ic_track_changes_24px = (ImageView) findViewById(R.id.ic_track_changes_24px);
+		qiblat = (TextView) findViewById(R.id.qiblat);
+		akun = (TextView) findViewById(R.id.akun);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
